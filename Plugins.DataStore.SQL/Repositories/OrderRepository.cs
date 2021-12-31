059e7f723b8ee76e3a01f043acdd10683cf96b3a
Valeri Panev <passionser@gmail.com>
2021-12-31 11:23:38 +0200

    Add Order and Supplier repositories

---
+++ Plugins.DataStore.SQL/Repositories/OrderRepository.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CoreBusiness;
+using Microsoft.EntityFrameworkCore;
+
+namespace Plugins.DataStore.SQL
+{
+    public class OrderRepository : IOrderRepository, IDisposable
+    {
+        private Dre6nikAppContext context;
+
+        public OrderRepository(Dre6nikAppContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<Order> GetOrders()
+        {
+            return context.Orders.ToList();
+        }
+
+        public void AddOrder(Order order)
+        {
+            context.Orders.Add(order);
+        }
+
+        public void UpdateOrder(Order order)
+        {
+            context.Entry(order).State = EntityState.Modified;
+        }
+
+        public Order GetOrderById(int orderId)
+        {
+            return context.Orders.Find(orderId);
+        }
+
+        public void DeleteOrder(int orderId)
+        {
+            Order order = context.Orders.Find(orderId);
+            context.Orders.Remove(order);
+        }
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+
+            this.disposed = true;
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ Plugins.DataStore.SQL/Repositories/SupplierRepository.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CoreBusiness;
+using Microsoft.EntityFrameworkCore;
+
+namespace Plugins.DataStore.SQL
+{
+    public class SupplierRepository : ISupplierRepository, IDisposable
+    {
+        private Dre6nikAppContext context;
+
+        public SupplierRepository(Dre6nikAppContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<Supplier> GetSuppliers()
+        {
+            return context.Suppliers.ToList();
+        }
+
+        public void AddSupplier(Supplier supplier)
+        {
+            context.Suppliers.Add(supplier);
+        }
+
+        public void UpdateSupplier(Supplier supplier)
+        {
+            context.Entry(supplier).State = EntityState.Modified;
+        }
+
+        public Supplier GetSupplierById(int supplierId)
+        {
+            return context.Suppliers.Find(supplierId);
+        }
+
+        public void DeleteSupplier(int supplierId)
+        {
+            Supplier supplier = context.Suppliers.Find(supplierId);
+            context.Suppliers.Remove(supplier);
+        }
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+
+            this.disposed = true;
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
